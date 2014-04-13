5c1c8f2ad7b14fd83df2774ad545fd90828959d1
JCPlusPlus <debugging.compiling@gmail.com>
2014-04-13 17:48:04 -0400

    Prototype 1
    Testing to see if I got github to work

---
+++ Unity 2D first proj/Assets/Scripts/WallScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+	public CharacterController charcontroller;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
